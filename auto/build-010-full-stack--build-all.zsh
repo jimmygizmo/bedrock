b4582025140e2a8c57d4bb2697887e3a69882d9d
Jimmy Gizmo <1915982+jimmygizmo@users.noreply.github.com>
2025-06-15 09:12:57 -0700

    seed data info

--- auto/build-010-full-stack--build-all.zsh
+++ auto/build-010-full-stack--build-all.zsh
@@ -11,12 +11,15 @@ echo
 # Echo all subsequent commands before executing them
 set -x
 
-# Build the postgres container.
+# Build the PostgreSQL [postgres] container.
 ./auto/build-020-docker-build-tag--postgres.zsh
 
-# Build the pgadmin container.
+# Build the PgAdmin4 [pgadmin] container.
 ./auto/build-030-docker-build-tag--pgadmin.zsh
 
-# Build the Bedrock FastAPI container.
+# Build the Bedrock FastAPI [bedrock] container running Magma microservices (FastAPI application).
 ./auto/build-040-docker-build-tag--bedrock.zsh
+
+# Build the Bedrock Jupyter Notebook Server [notebook] container.
+./auto/build-050-docker-build-tag--notebook.zsh
 
